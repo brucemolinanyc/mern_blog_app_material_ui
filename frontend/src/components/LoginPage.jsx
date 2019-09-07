eabbac42126ba8e8cdf389bf300d42e0ebb55ee4
brucemolinanyc <brucemolinanyc@gmail.com>
2019-09-07 12:40:35 -0400

    started adjusting the front end to meet backend needs

---
+++ frontend/src/components/LoginPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class LoginPage extends React.Component{
+ 
+    state = {
+        email: '',
+        password: ''
+    }
+    
+    render(){
+        return(
+            <div>
+                This is the login page
+            </div>
+        )
+    }
+
+};
+
+export default LoginPage;
